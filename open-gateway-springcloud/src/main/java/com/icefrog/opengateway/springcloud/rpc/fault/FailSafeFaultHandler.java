5356199e208b90d70af8f953045fe80d22d0e9f5
icefrog <icefrog.su@qq.com>
2020-03-04 00:26:33 +0800

    故障处理实现

---
+++ open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/fault/FailSafeFaultHandler.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2020 icefrog All rights reserved.
+ *
+ * @since 1.8
+ * @author: icefrog.su@qq.com
+ */
+
+package com.icefrog.opengateway.springcloud.rpc.fault;
+
+import com.icefrog.opengateway.common.base.RpcException;
+import com.icefrog.opengateway.springcloud.core.Response;
+import com.icefrog.opengateway.springcloud.rpc.AbstractFaultHandler;
+import com.icefrog.opengateway.springcloud.rpc.Invoke;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author IceFrog
+ */
+public class FailSafeFaultHandler extends AbstractFaultHandler implements Invoke<Response> {
+
+    @Override
+    public Response invoke(RestTemplate restTemplate) throws RpcException {
+        return null;
+    }
+}
