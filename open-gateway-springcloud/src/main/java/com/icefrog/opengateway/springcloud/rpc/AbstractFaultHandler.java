e2736599562ec38d386c3bb82c7baeb1fc3f7695
icefrog <icefrog.su@qq.com>
2020-03-04 00:26:57 +0800

    抽象故障处理

---
+++ open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/AbstractFaultHandler.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2020 icefrog All rights reserved.
+ *
+ * @since 1.8
+ * @author: icefrog.su@qq.com
+ */
+
+package com.icefrog.opengateway.springcloud.rpc;
+
+/**
+ * @author IceFrog
+ */
+public abstract class AbstractFaultHandler {
+}
