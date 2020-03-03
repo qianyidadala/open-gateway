4d62122cdc34ca9b4832dbf966b291ebda7bd3b1
icefrog <icefrog.su@qq.com>
2020-03-04 00:27:35 +0800

    分支OpenGateway两种主分支异常类型

---
+++ open-gateway-common/src/main/java/com/icefrog/opengateway/common/base/RpcException.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2020 icefrog All rights reserved.
+ *
+ * @since 1.8
+ * @author: icefrog.su@qq.com
+ */
+
+package com.icefrog.opengateway.common.base;
+
+/**
+ * @author IceFrog
+ */
+public class RpcException extends Exception {
+}
