eb37fd21f21ac16be8b3aab4c823ec4ee9bf4a63
icefrog <icefrog.su@qq.com>
2020-03-04 21:19:50 +0800

    故障处理实现类

--- open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/fault/FailOverFaultHandler.java
+++ open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/fault/FailOverFaultHandler.java
@@ -11,12 +11,17 @@ import com.icefrog.opengateway.common.base.RpcException;
 import com.icefrog.opengateway.springcloud.core.Response;
 import com.icefrog.opengateway.springcloud.rpc.AbstractFaultHandler;
 import com.icefrog.opengateway.springcloud.rpc.Invoke;
+import com.icefrog.opengateway.springcloud.rpc.RpcContext;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author IceFrog
  */
 public class FailOverFaultHandler extends AbstractFaultHandler implements Invoke<Response> {
+
+    public FailOverFaultHandler(RpcContext context) {
+        super(context);
+    }
 
     @Override
     public Response invoke(RestTemplate restTemplate) throws RpcException {
