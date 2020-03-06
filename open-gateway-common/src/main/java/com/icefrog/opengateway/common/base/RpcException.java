9fb593ca867b3f57dc2f4187f0011c90c24a1448
icefrog <icefrog.su@qq.com>
2020-03-06 15:44:56 +0800

    rpc exception

--- open-gateway-common/src/main/java/com/icefrog/opengateway/common/base/RpcException.java
+++ open-gateway-common/src/main/java/com/icefrog/opengateway/common/base/RpcException.java
@@ -11,4 +11,16 @@ package com.icefrog.opengateway.common.base;
  * @author IceFrog
  */
 public class RpcException extends Exception {
+
+    public RpcException(String message) {
+        super(message);
+    }
+
+    public RpcException(Throwable t) {
+        super(t);
+    }
+
+    public RpcException(String message, Throwable t) {
+        super(message, t);
+    }
 }
