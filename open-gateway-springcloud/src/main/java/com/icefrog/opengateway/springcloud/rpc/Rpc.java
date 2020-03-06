ae324d902e1661b10dc86b2c1461f0d05f6bd6bf
icefrog <icefrog.su@qq.com>
2020-03-06 15:43:22 +0800

    remote the rpc class code

--- open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/Rpc.java
+++ open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/Rpc.java
@@ -7,45 +7,34 @@
 
 package com.icefrog.opengateway.springcloud.rpc;
 
-import com.icefrog.opengateway.common.base.HttpProtocol;
 import com.icefrog.opengateway.common.base.RpcException;
 import com.icefrog.opengateway.springcloud.core.Response;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 
 /**
  * @author IceFrog
  */
 @Slf4j
 @Setter
-public class Rpc extends AbstractFaultHandler implements Invoke<Response> {
+public class Rpc extends AbstractFaultRpcHandler {
 
-    private AbstractFaultHandler faultHandler;
+    private AbstractFaultRpcHandler faultHandler;
+
+    protected Rpc(RpcContext rpcContext) {
+        super(rpcContext);
+    }
+
 
     @Override
-    public Response invoke(RestTemplate restTemplate) throws RpcException {
-        int currentIndex = 0;
-        while(currentIndex < rpcContext.getRetryCount()) {
-            try {
-                currentIndex++;
-                ResponseEntity<String> response = restTemplate.getForEntity(new URI(rpcContext.getProtocol() + rpcContext.getServiceId() + rpcContext.getUri()), String.class);
-                String respStr = response.getBody();
-                System.out.println(respStr);
-                break;
-            } catch (Exception e) {
-                if(currentIndex == rpcContext.getRetryCount()) {
-                    log.error("rpc invoke failed! count to:%d", currentIndex);
-                    break;
-                }
-                if(log.isWarnEnabled()) {
-                    log.warn("rpc invoke failed! retry:%d, current:%d, message:%s", rpcContext.getRetryCount(), currentIndex, e.getMessage());
-                }
-            }
-        }
+    public Response doInvoke(RestTemplate template) throws RpcException {
+        return null;
+    }
+
+    @Override
+    public Response doInvokeAsync(RestTemplate template) throws RpcException {
         return null;
     }
 }
