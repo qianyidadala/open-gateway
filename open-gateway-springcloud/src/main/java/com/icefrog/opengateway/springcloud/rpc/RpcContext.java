03f5d41a6ad913949f29786d0869c10668ab5089
icefrog <icefrog.su@qq.com>
2020-03-06 15:44:31 +0800

    rpc context add config

--- open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/RpcContext.java
+++ open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/RpcContext.java
@@ -8,7 +8,10 @@
 package com.icefrog.opengateway.springcloud.rpc;
 
 import com.icefrog.opengateway.common.base.HttpProtocol;
+import com.icefrog.opengateway.springcloud.core.OpenGatewayContext;
 import lombok.Data;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author IceFrog
@@ -27,6 +30,10 @@ public class RpcContext {
     private Integer timeout;
 
     private String protocol;
+
+    private HttpServletRequest request;
+
+    private OpenGatewayContext openGatewayContext;
 
     public void setProtocol(HttpProtocol protocol) {
         this.protocol = protocol.getValue() + "://";
