49c2398ed7bd4009a5d3fd6837e88a4cec1f5a52
icefrog <icefrog.su@qq.com>
2020-03-06 11:00:08 +0800

    快速失败故障策略实现

--- open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/fault/FailFastFaultHandler.java
+++ open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/fault/FailFastFaultHandler.java
@@ -11,9 +11,17 @@ import com.icefrog.opengateway.common.base.RpcException;
 import com.icefrog.opengateway.springcloud.core.Response;
 import com.icefrog.opengateway.springcloud.rpc.AbstractFaultHandler;
 import com.icefrog.opengateway.springcloud.rpc.RpcContext;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+
 /**
+ * A quick failure, a failed call, throws an exception and does not retry.
+ * Implementations in asynchronous mode are consistent with synchronous implementations
+ *
+ * @see com.icefrog.opengateway.springcloud.rpc.AbstractFaultHandler
+ * @see com.icefrog.opengateway.springcloud.rpc.Invoke
  * @author IceFrog
  */
 public class FailFastFaultHandler extends AbstractFaultHandler {
@@ -24,13 +32,21 @@ public class FailFastFaultHandler extends AbstractFaultHandler {
 
     @Override
     public Response doInvoke(RestTemplate template) throws RpcException {
-        return null;
+        String url;
+        ResponseEntity<Response> response;
+        try {
+            url = rpcContext.getProtocol() + rpcContext.getServiceId() + rpcContext.getUri();
+            response = template.getForEntity(new URI(url), Response.class);
+        } catch (Exception e) {
+            throw new RpcException(String.format("invoke service %s failed, trigger the FailFast strategy! error-message:%s",
+                    rpcContext.getServiceId(), e.getMessage()),
+                    e.getCause());
+        }
+        return response.getBody();
     }
 
     @Override
     public Response doInvokeAsync(RestTemplate template) throws RpcException {
-        return null;
+        return this.doInvoke(template);
     }
-
-
 }
