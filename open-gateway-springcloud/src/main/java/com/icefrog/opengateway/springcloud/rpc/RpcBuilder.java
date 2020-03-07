6f4ba528cdb4b693e37dc81fd677bd270a1658a3
icefrog <icefrog.su@qq.com>
2020-03-08 00:03:44 +0800

    重构rpc builder

--- open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/RpcBuilder.java
+++ open-gateway-springcloud/src/main/java/com/icefrog/opengateway/springcloud/rpc/RpcBuilder.java
@@ -7,26 +7,79 @@
 
 package com.icefrog.opengateway.springcloud.rpc;
 
+import com.icefrog.opengateway.common.base.FaultHandlerEnum;
 import com.icefrog.opengateway.common.base.HttpProtocol;
+import com.icefrog.opengateway.springcloud.core.OpenGatewayContext;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author IceFrog
  */
 public class RpcBuilder {
 
-    private Rpc rpc;
+    private AbstractFaultRpcHandler faultRpcHandler;
 
-    public Rpc init() {
-        if(rpc == null) {
-            rpc = new Rpc();
+    private RpcContext rpcContext;
+
+    public static RpcBuilder newInstance() {
+        return new RpcBuilder();
+    }
+
+    public AbstractFaultRpcHandler getRpcHandler(FaultHandlerEnum faultHandlerType) {
+        return FaultHandlerFactory.newInstance(rpcContext).getFaultHandler(faultHandlerType);
+    }
+
+    private RpcContext ensure() {
+        if(rpcContext == null) {
+            rpcContext = new RpcContext();
         }
-        return rpc;
+        return rpcContext;
     }
 
-    public Rpc getRpc() {
-        return this.rpc;
+    public RpcBuilder setServiceId(String serviceId) {
+        ensure().setServiceId(serviceId);
+        return this;
     }
 
+    public RpcBuilder setUri(String uri, boolean parseServiceId) {
+        this.ensure().setUri(uri);
+        if(parseServiceId) {
+            uri = uri.replaceAll("/", ",");
+            String serviceId = uri.split(",")[1];
+            this.ensure().setServiceId(serviceId);
+        }
+        return this;
+    }
 
+    public RpcBuilder setRetryCount(Integer retryCount) {
+        ensure().setRetryCount(retryCount);
+        return this;
+    }
+
+    public RpcBuilder setAsync(boolean isAsync) {
+        ensure().setAsync(isAsync);
+        return this;
+    }
+
+    public RpcBuilder setTimeout(Integer timeout) {
+        ensure().setTimeout(timeout);
+        return this;
+    }
+
+    public RpcBuilder setProtocol(HttpProtocol protocol) {
+        ensure().setProtocol(protocol);
+        return this;
+    }
+
+    public RpcBuilder setRequest(HttpServletRequest request) {
+        ensure().setRequest(request);
+        return this;
+    }
+
+    public RpcBuilder setOpenGatewayContext(OpenGatewayContext openGatewayContext) {
+        ensure().setOpenGatewayContext(openGatewayContext);
+        return this;
+    }
 
 }
